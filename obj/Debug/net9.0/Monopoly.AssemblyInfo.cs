18b03c57982e70ea31879da5c3d2acd19b32c0da
Mihail <mishatupchi@gmail.com>
2026-02-21 20:05:11 +0300

    Добавлены тесты и CI

--- obj/Debug/net9.0/Monopoly.AssemblyInfo.cs
+++ obj/Debug/net9.0/Monopoly.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Monopoly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38a4c77ebb61c44efe0059ba422c7b202a105681")]
 [assembly: System.Reflection.AssemblyProductAttribute("Monopoly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Monopoly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
